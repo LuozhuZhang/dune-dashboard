5e43d1aecdb48c5bbc45e034608718ea62adf10e
Mickey <hedgefund996@gmail.com>
2022-06-06 20:37:39 +0800

    calldata size & rollup L1 contract
    几个主流的rollup
    还有一些gamefi和NFT的market

--- calldata-size.sql
+++ calldata-size.sql
@@ -7,3 +7,13 @@ with call_data as (
 
 -- 一段时间内的平均calldata size
 select avg(octet_length(data)) / 1024.0 as avg_calldata_kb from call_data
+
+-- * 2.不同合约的calldata size
+-- 比如这个就是Optimism
+select "block_time", sum(octet_length(data)) / 1024.0 as calldata_kb from ethereum.transactions
+where block_time > now() - interval '10day'
+and "to" = '\xbe5dab4a2e9cd0f27300db4ab94bee3a233aeb19'
+-- zksync：xabea9132b05a70803a4e85094fd0e1800777fbef
+-- arbiturm：x4c6f947ae67f572afa4ae0730947de7c874f95ef
+group by block_time
+order by "block_time"
